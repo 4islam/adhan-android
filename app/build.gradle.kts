8e7e0925ae77cdc1780ba00b39fe6b3873e2cdd9
Naveed ul Islam <naveed@calgarycares.ca>
2026-01-26 12:12:49 -0700

    Back to working build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.adhan.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 36
-        versionName = "2.25"
+        versionCode = 52
+        versionName = "2.25.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
